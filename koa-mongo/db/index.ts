cb35071a0507ab2d2a97fd201758349e5a7b9611
Mikl Wolfe <wolfemikl@gmail.com>
2021-12-22 15:34:33 -0700

    Include dotenv

--- koa-mongo/db/index.ts
+++ koa-mongo/db/index.ts
@@ -1,7 +1,10 @@
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
 import modelPost from './models/Post';
 import modelUser from './models/User';
+
+dotenv.config();
 
 const container = 'mongo';
 const port = '27017';
@@ -10,20 +13,8 @@ const mongoURI = `mongodb://${process.env.DB_CONTAINER || container}:${
   process.env.DB_PORT || port
 }/${process.env.DB_NAME || database}`;
 
-export const db = mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  // useFindAndModify: false,
-  // useCreateIndex: true,
-});
-
-db.then(() => console.log(`Connected to mongo at: ${mongoURI}`)).catch(
-  (error) => {
-    console.error(
-      `There was a problem connecting to mongo at: ${mongoURI}`,
-      error,
-    );
-  },
+export const database_ = mongoose.connect(mongoURI, {}, () =>
+  console.log(`Connected to mongo at: ${mongoURI}`),
 );
 
 export const User = mongoose.model('User', modelUser);
